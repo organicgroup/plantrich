c015e572f8b34ff9eeb27c40ae6b5e6a92f8c3e6
BibinVinoy <bibincv237@gmail.com>
2023-11-01 00:30:50 +0530

    bugfix:feedback table

--- plantrich/public/js/lead.js
+++ plantrich/public/js/lead.js
@@ -1,12 +1,11 @@
 frappe.ui.form.on("Lead", {
     before_save:function(frm){
         let total_amount = 0
-        frm.doc.custom_item.forEach(item=>{
+        frm.doc.custom_item.forEach(row=>{
             if(!row.amount){
                 row.amount = row.qty * row.rate
             }
-            total_amount += item.amount
-            
+            total_amount += row.amount 
         })
         frm.set_value("custom_total_amount", total_amount)
     }
@@ -18,6 +17,15 @@ frappe.ui.form.on('Lead Item', {
         row.qty = 1
         row.amount = row.qty * row.rate
         frm.refresh_field("custom_item")
+        if(row.rate == 0){
+            frappe.db.get_value("Item Price", {"item_code": row.item_code, "price_list": "Standard Selling", "selling":1}, ["price_list_rate"]).then(data=>{
+                if(data.message.price_list_rate){
+                    row.rate = data.message.price_list_rate
+                    row.amount = data.message.price_list_rate * row.qty
+                    frm.refresh_field("custom_item")
+                }
+            })
+        }
         
     },
     qty:function(frm, cdt, cdn){
