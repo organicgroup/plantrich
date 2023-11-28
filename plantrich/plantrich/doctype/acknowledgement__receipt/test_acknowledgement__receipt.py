09d7acd811fcfd130784e22b05e5631ed152ad0c
sammish <sammish.thundiyil@gmail.com>
2023-11-28 06:50:05 +0000

    update

--- plantrich/plantrich/doctype/acknowledgement__receipt/test_acknowledgement__receipt.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, sammish and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestAcknowledgementReceipt(FrappeTestCase):
-	pass
--- plantrich/plantrich/doctype/previous_audit_closing_report_details/previous_audit_closing_report_details.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, sammish and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.utils.nestedset import NestedSet
-
-class PreviousauditClosingReportDetails(NestedSet):
-	pass
