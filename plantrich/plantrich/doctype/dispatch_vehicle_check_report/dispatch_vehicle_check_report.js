def4aa942e34c0432889f2bc11d7fc75ca716eea
Nijina <nijina.a@acube.co>
2024-08-31 19:43:58 +0530

    task customizations

---
+++ plantrich/plantrich/doctype/dispatch_vehicle_check_report/dispatch_vehicle_check_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, sammish and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Dispatch Vehicle Check Report', {
+	// refresh: function(frm) {
+
+	// }
+});
