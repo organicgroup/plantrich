cc0fc635cd08bc6bf7430a8c2c2b27938f19e91c
Ajmal <ajmalkomath600@gmail.com>
2026-02-28 13:10:08 +0530

    Sampling Register updation

--- plantrich/plantrich/doctype/sampling_register/sampling_register.js
+++ plantrich/plantrich/doctype/sampling_register/sampling_register.js
@@ -6,3 +6,21 @@
 
 // 	},
 // });
+frappe.ui.form.on('Sampling Register Details', {
+    no_of_bags: function(frm, cdt, cdn) {
+        calculate_total(frm, cdt, cdn);
+    },
+
+    bulk_sample: function(frm, cdt, cdn) {
+        calculate_total(frm, cdt, cdn);
+    }
+});
+
+function calculate_total(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+
+    let no_of_bags = row.no_of_bags || 0;
+    let bulk_sample = row.bulk_sample || 0;
+
+    frappe.model.set_value(cdt, cdn, 'total_qty_of_sample', no_of_bags * bulk_sample);
+}
