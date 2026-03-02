71d2a4b4dc1db40b9791c036552e68df72286f0b
Mohammed Ajmal K <ajmalkomath600@gmail.com>
2026-03-02 17:20:14 +0530

    sampling register updation

--- plantrich/plantrich/doctype/sampling_register/sampling_register.js
+++ plantrich/plantrich/doctype/sampling_register/sampling_register.js
@@ -24,3 +24,26 @@ function calculate_total(frm, cdt, cdn) {
 
     frappe.model.set_value(cdt, cdn, 'total_qty_of_sample', no_of_bags * bulk_sample);
 }
+
+
+frappe.ui.form.on('Sampling Register Details', {
+
+    total_qty: function(frm, cdt, cdn) {
+        calculate_balance(frm, cdt, cdn);
+    },
+
+    dispatched_qty: function(frm, cdt, cdn) {
+        calculate_balance(frm, cdt, cdn);
+    }
+});
+
+function calculate_balance(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+
+    let total = flt(row.total_qty) || 0;
+    let dispatched = flt(row.dispatched_qty) || 0;
+
+    row.balance_lot_qty = total - dispatched;
+
+    frm.refresh_field("details");  // replace with your child table fieldname if different
+}
