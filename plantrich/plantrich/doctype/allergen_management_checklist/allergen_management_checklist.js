b78814d8b6b8540dd796d73002140effe777eb4c
Mohammed Ajmal K <ajmalkomath600@gmail.com>
2026-08-27 17:02:58 +0530

    Allergen Management Doctype

---
+++ plantrich/plantrich/doctype/allergen_management_checklist/allergen_management_checklist.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, sammish and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Allergen Management Checklist", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ plantrich/plantrich/doctype/allergen_management_checklist/test_allergen_management_checklist.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, sammish and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAllergenManagementChecklist(FrappeTestCase):
+	pass
